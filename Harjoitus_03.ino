131b4d7badfba442f9d97c0c3ee8261734eec980
Roope Saario <roope.saario@ictpro.labra>
2020-11-18 13:32:09 +0200

    Tupla IF

--- Harjoitus_03.ino
+++ Harjoitus_03.ino
@@ -2,13 +2,25 @@
  * Ohjelmassa luetaan arjaporttiin tulleita arvoja. Luettu arvo 
  * tallentuu data nimiseen muuttujaan.
  */
+
+#define LED 5
+ 
 void setup() {
+  pinMode(LED, OUTPUT);
   Serial.begin(9600);
 }
+
+#define LEDON LOW
+#define LEDOFF HIGH
 
 void loop() {
   int data = Serial.read();
   if(data != -1){
   Serial.println(data);
+  if(data == 'S'){
+    digitalWrite(LED, LEDON);
+  }else if(data == 'R'){
+    digitalWrite(LED, LEDOFF);
+   }
   }
 }
